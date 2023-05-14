773d50df3c07638a2c76a302cfe020f4e5c51442
Alexander Tarba <sachatarba@rambler.ru>
2023-05-14 12:50:53 +0300

    added move system

--- ECS/Systems/inc/MoveSystem.h
+++ ECS/Systems/inc/MoveSystem.h
@@ -2,13 +2,23 @@
 #define ECS_MOVESYSTEM_H
 
 #include "BaseSystem.h"
-
+#include "ECS.h"
+#include "PositionComponent.h"
+#include "VelocityComponent.h"
 
 class MoveSystem : public BaseSystem {
 public:
     virtual int getSystemId() override;
 
-    virtual void update(EntityManager* manager) override;
+    virtual void update(Ent;ityManager* manager) override {
+        std::vector<Entity*> entyties;
+        manager->selectEntites<PositionComponent, VelocityComponent>(entyties);
+
+        for (e: entyties) {
+            e->getComponent<PositionComponent>.position += e->getComponent<VelocityComponent>.velocity;
+        }
+
+    }
 
     virtual bool added() override;
 
