a3e64ffb2061cdbd59b859f196ee4b10906ebfb9
1qwaka <yalenin01@gmail.com>
2023-05-14 14:07:13 +0300

    events system added

---
+++ events/events.cpp
@@ -0,0 +1,17 @@
+#include "events.h"
+
+
+// void Events::on(EventType type, EventHandler handler)
+// {
+//     if (events.find(type) == events.end())
+//         events.insert({type, std::vector<EventHandler>()});
+    
+//     events[type].push_back(handler);
+// }
+
+
+// void Events::fire(EventType type, Event* event)
+// {
+//     for (auto &handler : events[type])
+//         handler(event);
+// }
